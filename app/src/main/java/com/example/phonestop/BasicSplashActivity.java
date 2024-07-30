df11dabdbcdb9155ff790a5120e11be966547a19
Nir Beja <90895452+Noogra@users.noreply.github.com>
2024-07-30 10:39:01 +0300

    Logo, Splash animation

---
+++ app/src/main/java/com/example/phonestop/BasicSplashActivity.java
@@ -0,0 +1,77 @@
+package com.example.phonestop;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BasicSplashActivity extends AppCompatActivity {
+    private AppCompatImageView splash_IMG_logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_splash);
+
+        findViews();
+        starAnimation(splash_IMG_logo);
+    }
+
+    private void starAnimation(View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+
+        //set Start Location:
+        view.setY(-height/2.0f);
+        view.setScaleX(0.0f);
+        view.setScaleY(0.0f);
+        view.setAlpha(0.0f);    //opacity
+
+        view.animate()
+                .alpha(1.0f)
+                .scaleX(1.0f)
+                .scaleY(1.0f)
+                .translationY(0)
+                .setDuration(4000)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(@NonNull Animator animation) {
+                       //pass
+                    }
+
+                    @Override
+                    public void onAnimationEnd(@NonNull Animator animation) {
+                        transactToMainActivity();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(@NonNull Animator animation) {
+                        //pass
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(@NonNull Animator animation) {
+                        //pass
+                    }
+                });
+
+    }
+
+    private void transactToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void findViews() {
+        splash_IMG_logo = findViewById(R.id.splash_IMG_logo);
+    }
+}
